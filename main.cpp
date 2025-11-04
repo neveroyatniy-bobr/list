ccbf624e40db1f25ea3918ecc2809fea0cb79723
neveroyatniy-bobr <nicant2007@gmail.com>
2025-11-04 23:44:04 +0300

    Протестировал list

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "list.hpp"
 
+#include <stdlib.h>
+
 int main() {
     List list;
     
@@ -16,17 +18,26 @@ int main() {
     }
     
     size_t fective_next = 0;
-    VectorGet(&list.next, 0, &fective_next);
-    for (size_t i = 0; i < 5; i++) {
+    VectorGet(&list.next, 5, &fective_next);
+    for (size_t i = 0; i < 3; i++) {
        if (ListDeleteAt(&list, fective_next) != LIST_OK) {
             ListPrintError(&list, __FILE__, __LINE__);
             VectorDump(&list.prev, __FILE__, __LINE__);
             return 1;
         }
-        VectorGet(&list.next, 0, &fective_next);
+        VectorGet(&list.next, 5, &fective_next);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        if (ListInsertAfter(&list, 0, i) != LIST_OK) {
+            ListPrintError(&list, __FILE__, __LINE__);
+            return 1;
+        }
     }
 
     ListGraphDump(&list, __FILE__, __LINE__);
+
+    system("explorer.exe dump_file.html");
 
     if (ListDestroy(&list) != LIST_OK) {
         ListPrintError(&list, __FILE__, __LINE__);
